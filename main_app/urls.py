3f33a4c0d8f3ef21550e10edbd7386431610c9e7
btb0 <benburke2016@gmail.com>
2023-05-27 21:06:37 -0400

    adds penguin index

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
+    path('penguins/', views.penguins_index, name='penguins_index'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
 
+# Temporary Data until implementing models
+penguins = [
+    {'name': 'Nora', 'species': 'Adélie', 'description': 'cute and small', 'age': 3},
+    {'name': 'Walter', 'species': 'Emperor', 'description': 'a little tubby but still cute', 'age': 4},
+]
+
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
 
 def about(request):
     return render(request, 'about.html')
+
+def penguins_index(request):
+    return render(request, 'penguins/index.html', {
+        'penguins': penguins
+    })
